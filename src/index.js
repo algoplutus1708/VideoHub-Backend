37c1361037373025340b50ea4e2ece2a3df467db
Swastick Das <swastickdas95@gmail.com>
2024-08-13 00:35:47 +0530

    Refractor

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@
 import connectDB from "./db/index.js "
 
 //require('dotenv').config({path:'./env'})
-import dotenv from "dotenv" // This is an experimental feature so we have to config the package.json file. we have to config the dev script in package.json file.
+import dotenv from "dotenv" // This is an experimental feature so we have to config the package.json file. we have to config the 'dev' script in package.json file.
 dotenv.config({  //5th and 6th line are important for configuring dotenv using module type.
     path:'./env'
 })
 
 connectDB()
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed",err)
+})
 
 
 
